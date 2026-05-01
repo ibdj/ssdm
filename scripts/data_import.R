5d617b575d143cff15e7e467bac415a3b9e71a77
ibdj <ibdjacobsen@gmail.com>
2026-05-01 22:20:11 -0100

    importing env data

--- scripts/data_import.R
+++ scripts/data_import.R
@@ -13,3 +13,8 @@ tms_means <- tms |>
   reframe(temp_mean = mean(temp),
          mean_soilmoisture = mean(moisture_data_raw))
 
+samples_qgis <- read_csv("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/r_data/future_changes_data/data/samples_qgis.csv")
+
+names(samples_qgis)
+
+samples <- read_csv("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/r_data/future_changes_data/data/samples.csv")
