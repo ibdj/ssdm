6fb7074371e597b87d3641f7dea41531b4d23c48
ibdj <ibdjacobsen@gmail.com>
2026-05-01 22:11:58 -0100

    importing the tms data

--- scripts/data_import.R
+++ scripts/data_import.R
@@ -3,3 +3,13 @@
 library(tidyverse)
 
 #### loading the data ####
+
+tms <- readRDS("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/r_data/future_changes_data/tms_pivot.rds")
+
+names(tms)
+
+tms_means <- tms |> 
+  group_by(plot) |> 
+  reframe(temp_mean = mean(temp),
+         mean_soilmoisture = mean(moisture_data_raw))
+
