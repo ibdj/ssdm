17348d7ae7955acf4667798dc86949efa089b96f
ibdj <ibdjacobsen@gmail.com>
2026-05-02 17:52:02 -0100

    importing data

--- scripts/data_import.R
+++ scripts/data_import.R
@@ -1,20 +1,25 @@
 #### packages ####
 
 library(tidyverse)
+library(janitor)
 
 #### loading the data ####
 
-tms <- readRDS("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/r_data/future_changes_data/tms_pivot.rds")
-
-names(tms)
-
-tms_means <- tms |> 
+tms <- readRDS("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/r_data/future_changes_data/data/tms_pivot.rds") |> 
+  clean_names() |> 
   group_by(plot) |> 
-  reframe(temp_mean = mean(temp),
-         mean_soilmoisture = mean(moisture_data_raw))
+  reframe(temp_mean_tms = mean(temp),
+          mean_soilmoisture_tms = mean(moisture_data_raw))
 
-samples_qgis <- read_csv("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/r_data/future_changes_data/data/samples_qgis.csv")
+summary(tms)
+
+samples_qgis <- read_csv("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/r_data/future_changes_data/data/samples_qgis.csv") |> 
+  select(plot, X,Y,elevation, ndvi, ndwi) |> 
+  left_join(tms, by = "plot")
 
 names(samples_qgis)
 
 samples <- read_csv("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/r_data/future_changes_data/data/samples.csv")
+
+names(samples)
+
