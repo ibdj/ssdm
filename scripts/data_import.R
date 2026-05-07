80491846533b8e5ba8d82930fad4f236a9ad4fde
ibdj <ibdjacobsen@gmail.com>
2026-05-07 12:36:58 -0100

    processing bb to cover conversion

--- scripts/data_import.R
+++ scripts/data_import.R
@@ -3,6 +3,22 @@
 library(tidyverse)
 library(janitor)
 
+#### functions ####
+bb_to_cover <- function(x) {
+  dplyr::case_when(
+    startsWith(x, "5 (") ~ 87.5,
+    startsWith(x, "4 (") ~ 62.5,
+    startsWith(x, "3 (") ~ 37.5,
+    startsWith(x, "2 (") ~ 15.0,
+    startsWith(x, "1 (") ~ 2.5,
+    startsWith(x, "+ (") ~ 1.0,
+    startsWith(x, "r (") ~ 0.5,
+    startsWith(x, "i (") ~ 0.1,
+    x == "0" ~ 0.0,
+    TRUE ~ NA_real_
+  )
+}
+
 #### loading the data ####
 
 tms <- readRDS("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/r_data/future_changes_data/data/tms_pivot.rds") |> 
@@ -23,6 +39,60 @@ df_raw <- read_csv("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPla
   clean_names() |> 
   mutate(rowid = row_number())
 
+df_cover <- df_raw |> 
+ mutate(across(ends_with("_bb"), bb_to_cover)) |> 
+  mutate(total_cover = rowSums(across(ends_with("_bb")), na.rm = TRUE))
+
+summary(df_raw)
+  
+unique(df_raw$bare_ground_bb)
+
+#  select(-object_id,-global_id,-creation_date, -creator, -edit_date,-editor,-date,-starts_with("time_"),-veg_height_n, -veg_height_e, -veg_height_s, -veg_height_w, -starts_with("soil_"),-x17)
+
+abiotic_plot <- df_cover |> 
+  select(plot_name , veg_height_ave, bare_ground_bb, x, y, total_cover)
+  
+summary(abiotic_plot)
+
+species_cols <- df_raw |> 
+  select(ends_with("_bb")) |> 
+  names() |> 
+  str_remove("_bb$") |> 
+  keep(~ .x %in% names(df_raw))  # only keep if taxon column actually exists
+
+# Then run the mismatch check as before
+mismatches <- map_dfr(species_cols, function(sp) {
+  df_raw |> 
+    filter(!is.na(.data[[sp]]) & .data[[sp]] != "" & 
+             is.na(.data[[paste0(sp, "_bb")]])) |> 
+    mutate(species = sp, 
+           taxon_value = .data[[sp]],
+           bb_value = .data[[paste0(sp, "_bb")]]) |> 
+    select(rowid, species, taxon_value, bb_value)
+})
+
+mismatches
+
+n_taxa <- df_raw |>
+  select(matches("^taxon_\\d+$")) |>
+  ncol()
+
+df <- map(seq_len(n_taxa), \(number) {
+  taxon_col  <- sym(paste0("taxon_", number))
+  height_col <- sym(paste0("taxon_", number, "_height"))
+  bb_col     <- sym(paste0("taxon_", number, "_bb"))
+  
+  df_raw |>
+    select(1:31, !!taxon_col, !!height_col, !!bb_col, 74:78) |>
+    mutate(position = paste0("taxon_", number)) |>
+    rename(taxon  = !!taxon_col,
+           height = !!height_col,
+           bb     = !!bb_col)
+}) |>
+  list_rbind()
+
+df <- df |> 
+  mutate(taxon = as.factor(taxon))
 
 generate_dataframe <- function(number) {
   taxon_col <- sym(paste0("taxon_", number))
