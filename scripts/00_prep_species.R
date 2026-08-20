f897d4f72c6bbcb16a4d5ed098adcfefd751eb14
ibdj <ibdjacobsen@gmail.com>
2026-08-20 14:02:48 -0100

    also saved the species long

--- scripts/00_prep_species.R
+++ scripts/00_prep_species.R
@@ -191,6 +191,7 @@ species_matrix_cover <- species_matrix |>
 
 species_matrix_pa <- (species_matrix_cover > 0) * 1
 
+saveRDS(species_long, "data/species_long.rds")
 saveRDS(species_matrix_out, "data/species_matrix_cover.rds")
 saveRDS(species_matrix_pa, "data/species_matrix_pa.rds")
 
