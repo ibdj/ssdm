4511192d682e8b0513cdbc104c318ce11ca5e96c
ibdj <ibdjacobsen@gmail.com>
2026-08-25 21:43:54 -0100

    packes and extrating values

---
+++ scripts/03_projections.R
@@ -0,0 +1,17 @@
+#### packages ##################################################################
+
+library(tidyverse)
+library(terra)
+
+#### packages ##################################################################
+
+ssp_dirs <- c(ssp126 = "/Users/ibdj/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/CMIP6/CMIP6 - Mean temperature (T) Change deg C - Long Term (2081-2100) SSP1-2.6 (rel. to 1995-2014) - June to August (32 models)/map.tif",
+              ssp245 = "/Users/ibdj/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/CMIP6/CMIP6 - Mean temperature (T) Change deg C - Long Term (2081-2100) SSP2-4.5 (rel. to 1995-2014) - June to August (34 models)/map.tif",
+              ssp370 = "/Users/ibdj/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/CMIP6/CMIP6 - Mean temperature (T) Change deg C - Long Term (2081-2100) SSP3-7.0 (rel. to 1995-2014) - June to August (30 models)/map.tif",
+              ssp585 = "/Users/ibdj/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/02 Modelling future changes/data/CMIP6/CMIP6 - Mean temperature (T) Change deg C - Long Term (2081-2100) SSP5-8.5 (rel. to 1995-2014) - June to August (34 models)/map.tif")
+
+deltas <- sapply(ssp_dirs, \(d) {
+  terra::extract(rast(file.path(d, "map.tif")),
+                 matrix(c(-51.38079898859376, 64.13399461827079), ncol = 2))[1, 1]
+})
+deltas
